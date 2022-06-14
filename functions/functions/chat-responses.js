acb1973316c17620a0cfeac19554769908d75bb0
Mike Schmitt <mschmitt@twilio.com>
2022-06-14 08:08:21 -0500

    Moved responses to asset

--- functions/functions/chat-responses.js
+++ functions/functions/chat-responses.js
@@ -1,41 +1,17 @@
-exports.handler = function(context, event, callback) {
-   // Create a custom Twilio Response
+exports.handler = function (context, event, callback) {
+  // Create a custom Twilio Response
   const response = new Twilio.Response();
-
-  const questions = [
-    {
-      "section": "Sales",
-      "questions": [
-        {
-          "label": "Cost",
-          "text": "We have multiple billing options. You can pay yearly and save 10%"
-        },
-        {
-          "label": "Promotion",
-          "text": "We do offer discounts for our best customers."
-        }
-      ]
-    },
-    {
-      "section": "Support",
-      "questions": [
-        { "label": "Login", "text": "You can login at https://example.com" },
-        {
-          "label": "Forgot Password",
-          "text": "You can reset your password here at https://example.com/passwordreset"
-        }
-      ]
-    }
-  ];
+  const openResponsesJSON = Runtime.getAssets()["/responses.json"].open;
+  const responsesJSON = JSON.parse(openResponsesJSON());
 
   // Set the CORS headers to allow Flex to make an error-free HTTP request to this Function
-  response.appendHeader('Access-Control-Allow-Origin', '*');
-  response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
-  response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
-  response.appendHeader('Content-Type', 'application/json');
+  response.appendHeader("Access-Control-Allow-Origin", "*");
+  response.appendHeader("Access-Control-Allow-Methods", "OPTIONS, POST, GET");
+  response.appendHeader("Access-Control-Allow-Headers", "Content-Type");
+  response.appendHeader("Content-Type", "application/json");
 
-  response.setBody({data: questions});
-      
+  response.setBody({ data: responsesJSON.categories });
+
   // Return a success response using the callback function
   return callback(null, response);
 };
